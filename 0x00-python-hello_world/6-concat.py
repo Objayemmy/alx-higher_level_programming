a21c1393f95ec914006facc33182743a6d46e4b6
objayemmy <opeobajuluwa4@gmail.com>
2024-01-16 09:42:28 -0800

    0x00. Python - Hello, World

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,3 @@
+str1 = "Holberton"
+str2 = " School"
+print("Welcome to {}!".format(str1 + str2))
---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,3 @@
+original_str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+modified_str = original_str[38:66] + original_str[106:111] + original_str[:6]
+print(modified_str)
