6185469a470f0ef2ea6c96f64864ffb409f244c6
objayemmy <opeobajuluwa4@gmail.com>
2024-02-19 22:11:19 +0000

    Full rectangle

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+. Python - Inheritance, task 6 """
+
+
+class BaseGeometry:
+    """Intentionally empty area() method.
+
+    """
+    def area(self):
+        """Unimplemented, only raises exception to notify user.
+
+        """
+        raise Exception('area() is not implemented')
